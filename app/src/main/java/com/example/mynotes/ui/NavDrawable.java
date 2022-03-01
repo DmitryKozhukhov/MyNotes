7b423d299b378c18fe98e924d59a4d0142f3b001
DmitryKozhukhov <kodmse@mail.ru>
2022-03-02 03:48:42 +0700

    Сделал экшн бар и боковое навигационное меню.

---
+++ app/src/main/java/com/example/mynotes/ui/NavDrawable.java
@@ -0,0 +1,8 @@
+package com.example.mynotes.ui;
+
+import androidx.appcompat.widget.Toolbar;
+
+public interface NavDrawable {
+
+    void setAppBar(Toolbar toolbar);
+}
--- app/src/main/java/com/example/mynotes/ui/details/NoteDetailsFragment.java
+++ app/src/main/java/com/example/mynotes/ui/details/NoteDetailsFragment.java
@@ -1,18 +1,21 @@
 package com.example.mynotes.ui.details;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentResultListener;
 
 import com.example.mynotes.R;
 import com.example.mynotes.domain.Note;
-import com.example.mynotes.ui.list.NotesListFragment;
+import com.example.mynotes.ui.NavDrawable;
 
 public class NoteDetailsFragment extends Fragment {
 
@@ -38,8 +41,36 @@ public class NoteDetailsFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+
+        if (requireActivity() instanceof NavDrawable) {
+            ((NavDrawable) requireActivity()).setAppBar(toolbar);
+        }
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.action_share:
+                        Toast.makeText(requireContext(), "Share", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_add_photo:
+                        Toast.makeText(requireContext(), "Add a photo", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return false;
+            }
+        });
 
         eventNote = view.findViewById(R.id.event_note);
 
@@ -47,27 +78,16 @@ public class NoteDetailsFragment extends Fragment {
 
         dateOfCreation = view.findViewById(R.id.dateOfCreation);
 
-        view.findViewById(R.id.remove).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                getParentFragmentManager()
-                        .popBackStack();
-            }
-        });
-
-
         Bundle arguments = getArguments();
 
-        if (arguments != null && arguments.containsKey(ARG_NOTE)){
+        if (arguments != null && arguments.containsKey(ARG_NOTE)) {
             Note note = arguments.getParcelable(ARG_NOTE);
 
             updateNote(note);
         }
     }
 
-
-    private void updateNote(Note note){
+    private void updateNote(Note note) {
 
         eventNote.setImageResource(note.getImage());
 
